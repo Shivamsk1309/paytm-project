292aa2fb317fd1a01776f3623303165055507ffc
Shivam Kaushik <skshivam.1309@gmail.com>
2024-03-03 18:43:50 +0530

    connecting to backend

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-const InputBox = ({ label, placeholder }) => {
+const InputBox = ({ label, placeholder, onChange }) => {
   return (
     <>
       <label className="text-md text-left font-semibold text-black py-2">
         {label}
       </label>
       <input
+        onChange={onchange}
         type="email"
         className="bg-gray-50 border  border-gray-900 text-gray-900 text-md rounded-lg block w-full p-2 mb-3"
         placeholder={placeholder}
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import { BottomWarning } from "../components/BottomWarning";
 import { Button } from "../components/Button";
 import Heading from "../components/Heading";
 import InputBox from "../components/InputBox";
 import SubHeading from "../components/SubHeading";
+import axios from "axios";
 
 const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function formSubmit() {
+    const data = {
+      firstName,
+      lastName,
+      password,
+      username,
+    };
+    console.log("requst to backend");
+    const response = await axios.post(
+      "http://localhost:3000/api/v1/user/signup",
+      data
+    );
+  }
   return (
     <>
       <div className="bg-slate-400 h-screen flex justify-center">
@@ -12,12 +32,28 @@ const Signup = () => {
           <div className="rounded-lg bg-white w-80 p-2 h-max px-4 ">
             <Heading label={"Sign up"} />
             <SubHeading label={"Enter your infromation to create an account"} />
-            <InputBox placeholder="Shivam" label={"First Name"} />
-            <InputBox placeholder="Kaushik" label={"Last Name"} />
-            <InputBox placeholder="youremail@example.com" label={"Email"} />
-            <InputBox placeholder="123456" label={"Password"} />
+            <InputBox
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder="Shivam"
+              label={"First Name"}
+            />
+            <InputBox
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder="Kaushik"
+              label={"Last Name"}
+            />
+            <InputBox
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="youremail@example.com"
+              label={"Email"}
+            />
+            <InputBox
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="123456"
+              label={"Password"}
+            />
             <div className="pt-4">
-              <Button label={"Sign up"} />
+              <Button onclick={formSubmit} label={"Sign up"} />
             </div>
             <BottomWarning
               label={"Already have an account?"}
